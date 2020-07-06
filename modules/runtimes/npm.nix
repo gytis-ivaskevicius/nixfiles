111f8adc47bb68d5bd63901c8850c7955f3bf180
Gytis Ivaskevicius <gytis02.21@gmail.com>
2020-07-06 17:19:34 +0300

    Update npm paths

--- modules/runtimes/npm.nix
+++ modules/runtimes/npm.nix
@@ -2,4 +2,13 @@
 {
   environment.systemPackages = with pkgs; [ nodejs ]; 
   programs.npm.enable = true;
+  programs.npm.npmrc = ''
+prefix=''${XDG_DATA_HOME}/npm
+cache=''${XDG_CACHE_HOME}/npm
+tmp=''${XDG_RUNTIME_DIR}/npm
+init-module=''${XDG_CONFIG_HOME}/npm/config/npm-init.js
+init-license=MIT
+color=true
+  '';
+
 }
