8948b62dd87f4c5f53842ce30e55a3c26e0bd437
Gytis Ivaskevicius <gytis02.21@gmail.com>
2020-10-20 10:30:04 +0300

    Add jdk14

--- modules/java/default.nix
+++ modules/java/default.nix
@@ -3,6 +3,7 @@
 let
   jdk8 = pkgs.jdk8;
   jdk11 = pkgs.jdk11;
+  jdk14 = pkgs.jdk14;
   default_jdk = pkgs.jdk11;
 in {
   programs.java.enable = true;
@@ -11,17 +12,21 @@ in {
   environment.variables = {
     JAVA_HOME8 = jdk8.home;
     JAVA_HOME11 = jdk11.home;
+    JAVA_HOME14 = jdk14.home;
   };
 
   environment.shellAliases = {
     java8 = "${jdk8.home}/bin/java";
     java11 = "${jdk11.home}/bin/java";
+    java14 = "${jdk14.home}/bin/java";
   };
 
   environment.systemPackages = with pkgs; [
+    gradle
     jdk11
+    jdk14
     jdk8
     maven
-    gradle
+    visualvm
   ];
 }
