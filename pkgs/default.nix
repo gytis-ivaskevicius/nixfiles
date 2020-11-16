0dc2ff1ece01340b80a9d34d1a2619d31287ac9a
Gytis Ivaskevicius <gytis.ivaskevicius@insoft.lt>
2020-11-16 14:30:20 +0200

    Add g-pistol

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,5 @@
 self: super: rec {
-  preview = super.callPackage ./preview { };
+  g-pistol = super.callPackage ./g-pistol { };
   g-neovim = super.callPackage ./g-neovim { };
   g-termite = super.callPackage ./g-termite { };
   g-rofi = super.callPackage ./g-rofi { };
---
+++ pkgs/g-pistol/default.nix
@@ -0,0 +1,35 @@
+{ pkgs, pistol }:
+
+let
+  pistol-config = pkgs.writeText "pistol.conf" ''
+    ^(video|image|audio)/*            mediainfo %pistol-filename%
+    application/csv                   column -s, -t %pistol-filename%
+    application/json                  bat --paging=never --plain --theme=base16 --color=always %pistol-filename%
+    text/plain                        bat --paging=never --plain --theme=base16 --color=always %pistol-filename%
+
+    fpath .*.(kt|kts|properties)$     bat --paging=never --plain --theme=base16 --color=always %pistol-filename%
+    fpath .*.(pom|iml)$               bat --language xml --paging=never --plain --theme=base16 --color=always %pistol-filename%
+    fpath .*.(repositories|sha1)$     bat --language properties --paging=never --plain --theme=base16 --color=always %pistol-filename%
+    fpath .*.jmod                     jmod list %pistol-filename%
+    fpath gradlew                     bat --paging=never --plain --theme=base16 --color=always %pistol-filename%
+
+    fpath .*.(7z)$                    7z l %pistol-filename%
+    fpath .*.(iso)$                   iso-info --no-header -l %pistol-filename%
+    fpath .*.(o)$                     nm %pistol-filename% | less
+    fpath .*.(rar)$                   unrar l %pistol-filename%
+    fpath .*.(tar)$                   tar tf %pistol-filename%
+    fpath .*.(tar.bz2|tbz2)$          tar tjf %pistol-filename%
+    fpath .*.(tar.txz|txz)$           xz --list %pistol-filename%
+    fpath .*.(tgz|tar.gz)$            tar tzf %pistol-filename%
+    fpath .*.(zip|jar|war|ear|oxt)$   unzip -l %pistol-filename%
+    fpath .*.[1-8]$                   man %pistol-filename% | col -b
+  '';
+  wrapped = pkgs.writeShellScriptBin "pistol" "${pistol}/bin/pistol --config ${pistol-config} $@";
+in
+  pkgs.symlinkJoin {
+    name = "pistol";
+    paths = [
+      wrapped
+      pistol
+    ];
+  }
