0795a7a9eb49ec9fd7907d4f635286c2b2892da8
Gytis Ivaskevicius <gytis02.21@gmail.com>
2021-01-18 17:18:16 +0200

    Add nix-community cachix

--- bundles/cachix.nix
+++ bundles/cachix.nix
@@ -11,12 +11,14 @@
       "https://cachix.cachix.org"
       "https://gytix.cachix.org/"
       "https://jrestivo.cachix.org"
+      "https://nix-community.cachix.org"
     ];
     binaryCachePublicKeys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
       "gytix.cachix.org-1:JXNZBxYslCV/hAkfNvJgyxlWb8jRQRKc+M0h7AaFg7Y="
       "jrestivo.cachix.org-1:+jSOsXAAOEjs+DLkybZGQEEIbPG7gsKW1hPwseu03OE="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 }
