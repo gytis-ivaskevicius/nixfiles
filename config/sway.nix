0d736c6000fb522729422e5b395684eb7002fa90
Gytis Ivaskevicius <me@gytis.io>
2021-09-08 11:36:25 +0300

    Tweak sway config

--- config/sway.nix
+++ config/sway.nix
@@ -2,9 +2,26 @@
 
 {
 
-  programs.sway.enable = true;
-  programs.sway.wrapperFeatures.gtk = true;
-  programs.sway.extraPackages = [ ];
+  services.upower.enable = true;
+
+  programs.sway = {
+    enable = true;
+    extraPackages = [ ];
+    wrapperFeatures = {
+      base = true;
+      gtk = true;
+    };
+    extraSessionCommands = ''
+      export SDL_VIDEODRIVER=wayland
+      export QT_QPA_PLATFORM=wayland
+      export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+      export _JAVA_AWT_WM_NONREPARENTING=1
+      export SUDO_ASKPASS="${pkgs.ksshaskpass}/bin/ksshaskpass"
+      export SSH_ASKPASS="${pkgs.ksshaskpass}/bin/ksshaskpass"
+      export XDG_SESSION_TYPE=wayland
+      export XDG_CURRENT_DESKTOP=sway
+    '';
+  };
 
   security.rtkit.enable = true;
   services.pipewire = {
